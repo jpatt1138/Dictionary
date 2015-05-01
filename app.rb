729832d5fdd7e5cab200ed1b4a1724acec7a32b8
Jim <wrench1138@gmail.com>
2015-05-01 12:06:29 -0700

    passes second integrationspec, navigates back to index with new word listed

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word')
+require('./lib/definition')
 
 get('/') do
   @all_words = Word.all()
@@ -16,6 +17,7 @@ post('/add_word')do
 word = params.fetch('word')
 part_of_speach = params.fetch('part_of_speach')
 definition = params.fetch('definition')
-Word.new({:word => word, :definition => Definition.new({:part_of_speach => part_of_speach, :definition => definition})}).save()
+Word.new({:word => word, :definition => Definition.new({:part_of_speach => part_of_speach, :definition_read_out => definition})}).save()
+@all_words = Word.all()
 erb(:index)
 end
