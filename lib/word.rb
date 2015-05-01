0d674f24bbbfc7f4c7c4ae1d92947ed790c19f96
Jim <wrench1138@gmail.com>
2015-05-01 10:08:25 -0700

    add second spec to test add method

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,12 @@
 class Word
 
+  @@words = []
+
   attr_reader(:word, :definition)
 
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word)
     @definition = attributes.fetch(:definition)
   end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -13,4 +13,18 @@ describe(Word) do
       expect(test_word.word()).to(eq("goblin"))
     end
   end
+
+  describe ('.all') do
+    it("returns all elements in the array of words") do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  # describe('#save') do
+  #   it('takes a particular word and saves it in an array') do
+  #     test_word = Word.new({:word => "goblin", :definition => Definition.new({ :part_of_speach => "noun", :definition_read_out => "a small mischievieous creature"}) })
+  #     test_word.save()
+  #
+  #   end
+  # end
 end
