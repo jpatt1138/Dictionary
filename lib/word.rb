186c8ea6fe9fde88c8ec1efee2c736c846fc2fcf
Jim <wrench1138@gmail.com>
2015-05-01 10:16:54 -0700

    pass third spec to save words in array

--- lib/word.rb
+++ lib/word.rb
@@ -8,9 +8,12 @@ class Word
     @word = attributes.fetch(:word)
     @definition = attributes.fetch(:definition)
   end
- 
+
   define_singleton_method(:all) do
     @@words
   end
 
+  define_method(:save) do
+    @@words.push(self)
+  end
 end
